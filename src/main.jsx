e0b41067506f88cec7adf61325014bc2d15fe552
juanm <juanmangubat@gmail.com>
2026-01-04 18:51:02 +1100

    maing routing update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App.jsx'
+import CahPlayer from './pages/cah/Player.jsx'
+import CahHost from './pages/cah/Host.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}></Route>
+      <Route path='/cah' element={<CahPlayer/>}></Route>
+      <Route path='/cah/host' element={<CahHost/>}></Route>
+    </Routes>
+  </BrowserRouter>
 )
